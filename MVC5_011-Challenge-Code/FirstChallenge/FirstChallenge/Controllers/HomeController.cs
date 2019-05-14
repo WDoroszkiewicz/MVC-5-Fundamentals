c4f10c139868f2e8e42f37541e726934121294ea
Wojciech Doroszkiewicz <doroszkiewicz666@gmail.com>
2019-05-14 14:46:11 +0200

    Initial commit

---
+++ MVC5_011-Challenge-Code/FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using FirstChallenge.Models;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(FirstChallenge.Models.ComicBookManager.GetComicBooks());
+        }
+        public ActionResult Detail(int? id)
+        {
+            ComicBook comicBook;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            comicBook = ComicBookManager.GetComicBooks().Where(x => x.ComicBookId == id).FirstOrDefault();
+            if (comicBook == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(comicBook);
+        }
+    }
+}
